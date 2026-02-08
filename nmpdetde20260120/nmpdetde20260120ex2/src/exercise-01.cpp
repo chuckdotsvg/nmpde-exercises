8d64647f8ce30aabd47b1a3c1c73990240824ea9
Carlo Aspren Gines Lara <101257700+chuckdotsvg@users.noreply.github.com>
2026-02-08 20:23:04 +0100

    exercise 2 of last tde, my trial of implementation of unsteady stokes, needs to fix convergence

---
+++ nmpdetde20260120/nmpdetde20260120ex2/src/exercise-01.cpp
@@ -0,0 +1,22 @@
+#include "NavierStokes.hpp"
+
+// Main function.
+int main(int argc, char *argv[]) {
+  Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv);
+
+  const std::string mesh_file_name = "../mesh/mesh-pipe.msh";
+  const unsigned int degree_velocity = 2;
+  const unsigned int degree_pressure = 1;
+
+  const unsigned int mu = 1;
+  const double timestep = 0.1;
+  const double T = 1.0;
+  const double theta = 1;
+
+  NavierStokes problem(mesh_file_name, degree_velocity, degree_pressure, mu,
+                       timestep, T, theta);
+
+  problem.run();
+
+  return 0;
+}
